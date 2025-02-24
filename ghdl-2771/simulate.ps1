e348316e2d668b27963ae52cfa183048fb320c94
Mark Brett <Xcodo@users.noreply.github.com>
2025-02-24 21:31:32 +0000

    Improve demo

--- ghdl-2771/simulate.ps1
+++ ghdl-2771/simulate.ps1
@@ -1,14 +1,22 @@
 # Get the locations for the symlnk and real file
-$command = (Get-Command("ghdl")).Source
-Write-Host "ghdl command runs $command"
-$commandTarget = (Get-Item($command)).Target
-Write-Host "ghdl command is link to $commandTarget"
+$commandInPath = (Get-Command("ghdl")).Source
+Write-Host "ghdl command runs $commandInPath"
+$commandSymlinkTarget = (Get-Item($commandInPath)).Target
+if ($commandSymlinkTarget) {
+    Write-Host "ghdl command is link to $commandSymlinkTarget"
+} else {
+    Write-Host "ghdl command is not a symlink"
+}
 
 # Setup the demo
 $args = '-c', '--std=08', 'square_wave.vhd', '-r', 'square_wave'
 
-Write-Host "`n=== Run symlink ==="
-& $command $args
+if ($commandInPath) {
+    Write-Host "`n=== Run from path ==="
+    & $commandInPath $args
+}
 
-Write-Host "`n=== Run link target ==="
-& $commandTarget $args
+if ($commandSymlinkTarget) {
+    Write-Host "`n=== Run link target ==="
+    & $commandSymlinkTarget $args
+}
